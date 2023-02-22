71cfb4a74b79d98b0ef799dd552b632ee66e38be
ossama zaoute <o.zaoute@intel-think.com>
2023-02-22 22:30:48 +0100

    updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -216,24 +216,6 @@ export default function Home() {
                 <div className={styles.years_list}>{yearOptions}</div>
               )}
             </div>
-            {/* <select
-              name="month"
-              value={selectedDate.getMonth()}
-              onChange={handleDateChange}
-            >
-              <option value="0">January</option>
-              <option value="1">February</option>
-              <option value="2">March</option>
-              <option value="3">April</option>
-              <option value="4">May</option>
-              <option value="5">June</option>
-              <option value="6">July</option>
-              <option value="7">August</option>
-              <option value="8">September</option>
-              <option value="9">October</option>
-              <option value="10">November</option>
-              <option value="11">December</option>
-            </select> */}
           </div>
           <div className={styles.months_days}>
             <div className={styles.days}>
